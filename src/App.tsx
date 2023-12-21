e7bf4d7d6792f24f4e4724deb80f416a99b70d8e
AnandD17 <avdoddamani123@gmail.com>
2023-12-21 23:11:17 +0530

    login flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import Router from './Router/Router';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer 
+        autoClose={2000}
+        hideProgressBar={true}
+        />
       <Router/>
     </div>
   );
